7ae63098689822a1d22e6df0718f0d27bd10e863
trifunem <trifunem@students.zhaw.ch>
2020-04-04 16:10:39 +0200

    added more api tests

--- backend/src/user/api.test.ts
+++ backend/src/user/api.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { CREATED } from 'http-status-codes';
+import { CREATED, OK } from 'http-status-codes';
 
 import server from '../server';
 
@@ -19,15 +19,31 @@ describe('Test User api', () => {
   });
 
   it('Should create new User', async () => {
-    const response = await request(server.server).post('/profile').send({
+    const response = await request(server.server).post('/user').send({
+      id: '1',
       email: 'Test',
       display_name: 'TESTDISPLAY',
     });
 
     expect(response.status).toEqual(CREATED);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('email');
+    expect(response.body).toHaveProperty('display_name');
+    expect(response.body.id).toBe('1');
+    expect(response.body.email).toBe('Test');
+    expect(response.body.display_name).toBe('TESTDISPLAY');
+  });
+  it('Should find a User', async () => {
+    const response = await request(server.server).get('/user/1');
+    expect(response.status).toEqual(OK);
     expect(response.body).toHaveProperty('email');
     expect(response.body).toHaveProperty('display_name');
     expect(response.body.email).toBe('Test');
     expect(response.body.display_name).toBe('TESTDISPLAY');
   });
+  it('Should delete a User', async () => {
+    const response = await request(server.server).delete('/user/1');
+    expect(response.status).toEqual(OK);
+  });
+
 });
