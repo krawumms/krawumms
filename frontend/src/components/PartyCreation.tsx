6679ec674c596b9698e166313945c0c0abebc6b3
Joel Cuter <cuterjoe@students.zhaw.ch>
2020-04-05 16:42:28 +0200

    added form to frontend to create party

--- frontend/src/components/PartyCreation.tsx
+++ frontend/src/components/PartyCreation.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import Axios from 'axios';
 
-const PartyCreation: React.FunctionComponent = () => (
-  <form
-    onSubmit={() =>
-      Axios.post('http://localhost:6011/parties', {
-        name: 'Here should be the partyname',
-      })
-    }
-  >
-    <p>Party name: </p>
-    <input id="partyName" name="partyName" type="text" />
-    <button type="submit">Create Party</button>
-  </form>
-);
-
+const PartyCreation: React.FunctionComponent = () => {
+  const [partyName, setPartyName] = React.useState('');
+  return (
+    <form
+      onSubmit={() =>
+        Axios.post('http://localhost:6011/parties', {
+          name: partyName,
+        })
+      }
+    >
+      <p>Party name: </p>
+      <input id="partyName" name="partyName" type="text" onChange={(e) => setPartyName(e.target.value)} />
+      <button type="submit">Create Party</button>
+    </form>
+  );
+};
 export default PartyCreation;
