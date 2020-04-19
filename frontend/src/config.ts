043eaa8a2d63577db415b9b704031a1f868101ba
TheDadi <dario.difazio@axa-winterthur.ch>
2020-04-19 11:27:17 +0200

    remove unused config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,4 @@
 export default {
   apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:6001',
   uiBaseUrl: process.env.UI_BASE_URL || 'http://localhost:3000',
-  apiBridgeBaseUrl: process.env.UI_BASE_URL || 'http://localhost:3000/api',
 };
