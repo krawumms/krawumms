ad18f996672cd97a4f7010634961d3e8ec515032
Joel Cuter <werbungnervt@hotmail.com>
2020-05-12 15:07:22 +0200

    fixed footer in div and removed additional html tag

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -8,55 +8,51 @@ type Props = {
 };
 
 const Layout: React.FunctionComponent<Props> = ({ children, title = 'This is the default title' }) => (
-  <html lang="en">
+  <Box>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Box bg="#1bd363" w="100%" color="white" left="0" top="0" text-align="center">
-      <header>
-        <Menu closeOnSelect>
-          <MenuButton as={Button} bg="none" _hover={{ bg: 'none' }} _focus={{ shadow: 'none' }}>
-            <IconButton
-              aria-label="Menu"
-              icon="triangle-down"
-              bg="none"
-              _hover={{ bg: 'none' }}
-              _focus={{ shadow: 'none' }}
-            />
-          </MenuButton>
-          <MenuList bg="#1bd363">
-            <MenuGroup>
-              <Link href="/">
-                <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
-                  Home
-                </MenuItem>
-              </Link>
-              <Link href="/parties">
-                <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
-                  Party List
-                </MenuItem>
-              </Link>
-              <Link href="/player">
-                <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
-                  Party Player
-                </MenuItem>
-              </Link>
-            </MenuGroup>
-          </MenuList>
-        </Menu>
-      </header>
+      <Menu closeOnSelect>
+        <MenuButton as={Button} bg="none" _hover={{ bg: 'none' }} _focus={{ shadow: 'none' }}>
+          <IconButton
+            aria-label="Menu"
+            icon="triangle-down"
+            bg="none"
+            _hover={{ bg: 'none' }}
+            _focus={{ shadow: 'none' }}
+          />
+        </MenuButton>
+        <MenuList bg="#1bd363">
+          <MenuGroup>
+            <Link href="/">
+              <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
+                Home
+              </MenuItem>
+            </Link>
+            <Link href="/parties">
+              <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
+                Party List
+              </MenuItem>
+            </Link>
+            <Link href="/player">
+              <MenuItem _hover={{ color: 'black' }} _focus={{ bg: 'none' }}>
+                Party Player
+              </MenuItem>
+            </Link>
+          </MenuGroup>
+        </MenuList>
+      </Menu>
     </Box>
     <Box p="1em" pb="3em">
       {children}
     </Box>
-    <footer>
-      <Box bg="#1bd363" w="100%" p={4} color="white" position="fixed" left="0" bottom="0" text-align="center">
-        <p>Created with love</p>
-      </Box>
-    </footer>
-  </html>
+    <Box bg="#1bd363" w="100%" p={4} color="white" position="fixed" left="0" bottom="0" text-align="center">
+      <p>Created with love</p>
+    </Box>
+  </Box>
 );
 
 export default Layout;
