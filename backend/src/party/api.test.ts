bcc33f3bcc8eca7e82d669db41a3d78278aa3ef3
trifunem <trifunem@students.zhaw.ch>
2020-05-17 19:48:24 +0200

    added more tests for party and user api

--- backend/src/party/api.test.ts
+++ backend/src/party/api.test.ts
@@ -1,8 +1,11 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { OK, CREATED } from 'http-status-codes';
+import { OK, CREATED, NOT_FOUND } from 'http-status-codes';
 
 import server from '../server';
+
+const bearer =
+  'Bearer BQC4lQOBT-eQ9ZnECslf5lBGL-7S8P4xDdea03IURwueBRm2cUfxObyFbWNC0y0Ffqzh7qsrHiNhOcZ_Za8BaMnOfHfUP40I4O6gfsaJoc8kIAMc2S9bFNDLg_7jgqubmrL7yD5SAWT0Fx7ZzsQLIGDeW5YY5o6NuGsbww';
 
 describe('Test party api', () => {
   beforeAll(async () => {
@@ -14,26 +17,24 @@ describe('Test party api', () => {
   });
 
   beforeEach(async () => {
-    await request(server.server)
-      .post('/parties')
-      .set(
-        'Authorization',
-        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
-      )
-      .send({
-        id: '1',
-        name: 'first test party',
-      });
-    await request(server.server)
-      .post('/parties')
-      .set(
-        'Authorization',
-        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
-      )
-      .send({
-        id: '2',
-        name: 'second test party',
-      });
+    await request(server.server).post('/parties').set('Authorization', bearer).send({
+      id: '1',
+      name: 'first test party',
+    });
+    await request(server.server).post('/parties').set('Authorization', bearer).send({
+      id: '2',
+      name: 'second test party',
+    });
+    await request(server.server).post('/parties').set('Authorization', bearer).send({
+      id: '4',
+      name: 'fourth test party',
+    });
+    await request(server.server).put('/parties/1/playlist').set('x-krawumms-client', '1234').send({
+      id: 'song1',
+    });
+    await request(server.server).put('/parties/4/playlist').set('x-krawumms-client', '12').send({
+      id: 'song4',
+    });
   });
 
   afterAll(() => {
@@ -42,27 +43,25 @@ describe('Test party api', () => {
   });
 
   it('Should create new Party', async () => {
-    const response = await request(server.server)
-      .post('/parties')
-      .set(
-        'Authorization',
-        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
-      )
-      .send({
-        id: '3',
-        name: 'third test party',
-      });
+    const response = await request(server.server).post('/parties').set('Authorization', bearer).send({
+      id: '3',
+      name: 'third test party',
+    });
     expect(response.status).toEqual(CREATED);
     expect(response.body.id).toBe('3');
   });
+
+  it('Should create new Party', async () => {
+    const response = await request(server.server).post('/parties').set('Authorization', bearer).send({
+      id: '3',
+      name: 'third test party',
+    });
+    expect(response.status).toEqual(CREATED);
+    expect(response.body.id).toBe('3');
+  });
+
   it('Should return all Parties', async () => {
-    const response = await request(server.server)
-      .get('/parties')
-      .set(
-        'Authorization',
-        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
-      )
-      .send();
+    const response = await request(server.server).get('/parties').set('Authorization', bearer).send();
     expect(response.status).toEqual(OK);
     expect(response.body[0].id).toBe('1');
     expect(response.body[1].id).toBe('2');
@@ -70,16 +69,15 @@ describe('Test party api', () => {
   });
 
   it('Should return a party by code', async () => {
-    const partyResponse = await request(server.server)
-      .get('/parties')
-      .set(
-        'Authorization',
-        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
-      )
-      .send();
+    const partyResponse = await request(server.server).get('/parties').set('Authorization', bearer).send();
     const coderesponse = await request(server.server).get(`/parties/byCode/${partyResponse.body[0].code}`).send();
     expect(coderesponse.status).toEqual(OK);
     expect(coderesponse.body.id).toBe('1');
+  });
+
+  it('Should return NOT_FOUND for invalid Code', async () => {
+    const response = await request(server.server).get(`/parties/byCode/dfasdas123gd`).send();
+    expect(response.body).toEqual(NOT_FOUND);
   });
 
   it('Should return 404 for invalid ID in GET', async () => {
@@ -95,7 +93,7 @@ describe('Test party api', () => {
   });
 
   it('Should add song to playlist', async () => {
-    const response = await request(server.server).put('/parties/1/playlist').set('x-krawumms-client', '1234').send({
+    const response = await request(server.server).put('/parties/2/playlist').set('x-krawumms-client', '1234').send({
       id: 'test',
     });
     expect(response.status).toEqual(OK);
@@ -103,7 +101,7 @@ describe('Test party api', () => {
   });
 
   it('Should get songs from playlist', async () => {
-    const response = await request(server.server).get('/parties/1/playlist');
+    const response = await request(server.server).get('/parties/2/playlist');
     console.log(response.body);
     expect(response.body.length).toEqual(1);
     expect(response.status).toEqual(OK);
@@ -122,8 +120,35 @@ describe('Test party api', () => {
 
   it('Should return 404 for invalid ID in PUT', async () => {
     const response = await request(server.server).put('/parties/1111').set('Authorization', 'Bearer abcd').send();
-
     expect(response.status).toEqual(OK);
     expect(response.body).toEqual(404);
+  });
+
+  it('Should make an upvote on a track', async () => {
+    const response = await request(server.server)
+      .put('/parties/1/playlist/song1/up-vote')
+      .set('x-krawumms-client', '123456')
+      .send({
+        body: {
+          id: '1',
+          trackId: 'song1',
+        },
+      });
+    expect(response.body.playlist[0].votes).toEqual(['1234', '123456']);
+    expect(response.status).toEqual(OK);
+  });
+
+  it('Should make an downvote on a track', async () => {
+    const response = await request(server.server)
+      .put('/parties/4/playlist/song4/down-vote')
+      .set('x-krawumms-client', '12')
+      .send({
+        body: {
+          id: '4',
+          trackId: 'song4',
+        },
+      });
+    expect(response.body.playlist[0].votes).toEqual([]);
+    expect(response.status).toEqual(OK);
   });
 });
--- backend/src/user/api.test.ts
+++ backend/src/user/api.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { CREATED, OK } from 'http-status-codes';
+import {CREATED, NOT_FOUND, OK} from 'http-status-codes';
 
 import server from '../server';
 
+const bearer =
+  'Bearer BQC4lQOBT-eQ9ZnECslf5lBGL-7S8P4xDdea03IURwueBRm2cUfxObyFbWNC0y0Ffqzh7qsrHiNhOcZ_Za8BaMnOfHfUP40I4O6gfsaJoc8kIAMc2S9bFNDLg_7jgqubmrL7yD5SAWT0Fx7ZzsQLIGDeW5YY5o6NuGsbww';
 describe('Test User api', () => {
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URL, {
@@ -13,9 +15,23 @@ describe('Test User api', () => {
     await server.ready();
   });
 
+  beforeEach(async () => {
+    await request(server.server).post('/user').send({
+      id: '20',
+      email: 'test@email.com',
+      display_name: 'DisplayTest',
+    });
+  });
+
   afterAll(() => {
     mongoose.disconnect();
     server.close();
+  });
+
+  it('Should get Spotify User data', async () => {
+    const response = await request(server.server).get('/me').set('Authorization', bearer).send();
+    expect(response.status).toEqual(OK);
+    expect(response.body.product).toBe('premium');
   });
 
   it('Should create new User', async () => {
@@ -24,6 +40,7 @@ describe('Test User api', () => {
       email: 'Test',
       display_name: 'TESTDISPLAY',
     });
+
     expect(response.status).toEqual(CREATED);
     expect(response.body).toHaveProperty('id');
     expect(response.body).toHaveProperty('email');
@@ -32,16 +49,29 @@ describe('Test User api', () => {
     expect(response.body.email).toBe('Test');
     expect(response.body.display_name).toBe('TESTDISPLAY');
   });
+
   it('Should find a User', async () => {
-    const response = await request(server.server).get('/user/1');
+    const response = await request(server.server).get('/user/20');
     expect(response.status).toEqual(OK);
     expect(response.body).toHaveProperty('email');
     expect(response.body).toHaveProperty('display_name');
-    expect(response.body.email).toBe('Test');
-    expect(response.body.display_name).toBe('TESTDISPLAY');
+    expect(response.body.email).toBe('test@email.com');
+    expect(response.body.display_name).toBe('DisplayTest');
   });
+
+  it('Should return user NOT_FOUND', async () => {
+    const response = await request(server.server).get('/user/999999');
+    expect(response.body).toEqual(NOT_FOUND);
+  });
+
   it('Should delete a User', async () => {
-    const response = await request(server.server).delete('/user/1');
+    const response = await request(server.server).delete('/user/20');
+    expect(response.status).toEqual(OK);
+  });
+
+  it('Should not find user to delete', async () => {
+    const response = await request(server.server).delete('/user/30');
+    expect(response.body).toEqual(NOT_FOUND);
     expect(response.status).toEqual(OK);
   });
 });
