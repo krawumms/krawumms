1c8bb62b9b886c80b0fe3fdfc8fa48f0929daa40
trifunem <trifunem@students.zhaw.ch>
2020-05-17 17:50:44 +0200

    added party tests

--- backend/src/party/api.test.ts
+++ backend/src/party/api.test.ts
@@ -1,7 +1,6 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { OK } from 'http-status-codes';
-import { Party } from './model';
+import { OK, CREATED } from 'http-status-codes';
 
 import server from '../server';
 
@@ -12,10 +11,29 @@ describe('Test party api', () => {
       useUnifiedTopology: true,
     });
     await server.ready();
-    await Party.create({
-      id: '1',
-      name: 'test party',
-    });
+  });
+
+  beforeEach(async () => {
+    await request(server.server)
+      .post('/parties')
+      .set(
+        'Authorization',
+        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
+      )
+      .send({
+        id: '1',
+        name: 'first test party',
+      });
+    await request(server.server)
+      .post('/parties')
+      .set(
+        'Authorization',
+        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
+      )
+      .send({
+        id: '2',
+        name: 'second test party',
+      });
   });
 
   afterAll(() => {
@@ -23,30 +41,55 @@ describe('Test party api', () => {
     server.close();
   });
 
-  // xdescribe('Should create new Party', async () => {
-  //   const response = await request(server.server).post('/parties').set('Authorization', 'Bearer abcd').send({
-  //     id: '1',
-  //     name: 'test party',
-  //   });
-  //   expect(response.status).toEqual(CREATED);
-  //   expect(response.body).toHaveProperty('name');
-  // });
-  // xdescribe('Should return all Parties', async () => {
-  //   const response = await request(server.server).get('/parties').set('Authorization', 'Bearer abcd').send();
-  //   expect(response.status).toEqual(OK);
-  //   expect(response.body.length).toBeGreaterThan(0);
-  // });
+  it('Should create new Party', async () => {
+    const response = await request(server.server)
+      .post('/parties')
+      .set(
+        'Authorization',
+        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
+      )
+      .send({
+        id: '3',
+        name: 'third test party',
+      });
+    expect(response.status).toEqual(CREATED);
+    expect(response.body.id).toBe('3');
+  });
+  it('Should return all Parties', async () => {
+    const response = await request(server.server)
+      .get('/parties')
+      .set(
+        'Authorization',
+        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
+      )
+      .send();
+    expect(response.status).toEqual(OK);
+    expect(response.body[0].id).toBe('1');
+    expect(response.body[1].id).toBe('2');
+    console.log(response.body);
+  });
+
+  it('Should return a party by code', async () => {
+    const partyResponse = await request(server.server)
+      .get('/parties')
+      .set(
+        'Authorization',
+        'Bearer BQC3pQyrPNlmD0CDs4JOww9O4YaEW5Kw5urn_freTar-wMptqNVUufxEfXGNEs4LGagQNWSZXqaTDQMjECF7PcJpWQ0qKpL5EqGme-fzc8_IsoFrWmosp8_bM9Z0hz7_PAzcwFjG6fRbqx8tMffiBAxWcoSnVB3cs3YXdg',
+      )
+      .send();
+    const coderesponse = await request(server.server).get(`/parties/byCode/${partyResponse.body[0].code}`).send();
+    expect(coderesponse.status).toEqual(OK);
+    expect(coderesponse.body.id).toBe('1');
+  });
 
   it('Should return 404 for invalid ID in GET', async () => {
     const response = await request(server.server).get('/parties/1111').send();
-
     expect(response.status).toEqual(OK);
     expect(response.body).toEqual(404);
   });
 
   it('Should return 404 for invalid ID in DELETE', async () => {
     const response = await request(server.server).delete('/parties/1111').send();
-
     expect(response.status).toEqual(OK);
     expect(response.body).toEqual(404);
   });
